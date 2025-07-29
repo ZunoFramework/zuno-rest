38102c22a7fb49dbcbe87b310fd7d7d337da0c2b
Jose Joya <jose.joya@globant.com>
2025-07-29 00:18:10 -0500

    Fix: Disable LIBPSL

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,10 +1,17 @@
 
 # Add dependencies here
-find_path(LIBPSL_INCLUDE_DIR NAMES libpsl.h PATHS ENV LIBPSL_INCLUDE_DIR)
-find_library(LIBPSL_LIBRARY NAMES psl PATHS ENV LIBPSL_LIBRARY)
+# Deactivate libpsl in windows
+if (NOT WIN32)
+    find_path(LIBPSL_INCLUDE_DIR NAMES libpsl.h PATHS ENV LIBPSL_INCLUDE_DIR)
+    find_library(LIBPSL_LIBRARY NAMES psl PATHS ENV LIBPSL_LIBRARY)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Libpsl REQUIRED_VARS LIBPSL_INCLUDE_DIR LIBPSL_LIBRARY)
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Libpsl REQUIRED_VARS LIBPSL_INCLUDE_DIR LIBPSL_LIBRARY)
+endif()
+
+if(WIN32)
+    set(CURL_USE_LIBPSL OFF)
+endif()
 
 add_dependency(nlohmann_json https://github.com/nlohmann/json.git v3.11.2)
 add_dependency(libcurl https://github.com/curl/curl.git master)
