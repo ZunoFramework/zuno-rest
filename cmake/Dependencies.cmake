a89e91f25a32bd54d3be9fa241fa7ee52634ab6a
Jose Joya <jose.joya@globant.com>
2025-07-28 22:28:53 -0500

    Fix: Removed wrong link libraries in Dependency Mngm

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,9 +6,6 @@ find_library(LIBPSL_LIBRARY NAMES psl PATHS ENV LIBPSL_LIBRARY)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Libpsl REQUIRED_VARS LIBPSL_INCLUDE_DIR LIBPSL_LIBRARY)
 
-target_include_directories(zuno-rest PRIVATE ${LIBPSL_INCLUDE_DIR})
-target_link_libraries(zuno-rest PRIVATE ${LIBPSL_LIBRARY})
-
 add_dependency(nlohmann_json https://github.com/nlohmann/json.git v3.11.2)
 add_dependency(libcurl https://github.com/curl/curl.git master)
 
